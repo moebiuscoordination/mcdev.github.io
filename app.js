eb3183b4a4b6fd87e8ff94acef732f52d76a9221
moebiuscoordination <moebiuscoordination@gmail.com>
2024-06-19 20:44:00 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+// Configure CesiumJS access token
+Cesium.Ion.defaultAccessToken = 'YOUR_CESIUM_ION_ACCESS_TOKEN';
+
+// Initialize the Cesium Viewer
+const viewer = new Cesium.Viewer('cesiumContainer', {
+    imageryProvider: new Cesium.IonImageryProvider({ assetId: 2 }), // Bing Maps Aerial imagery
+    baseLayerPicker: false,
+    geocoder: false,
+    homeButton: false,
+    sceneModePicker: false,
+    timeline: false,
+    navigationHelpButton: false,
+    animation: false
+});
+
+// Enable lighting
+viewer.scene.globe.enableLighting = true;
+
+// Add some additional effects
+viewer.scene.postProcessStages.fxaa.enabled = true;
+viewer.scene.skyAtmosphere.show = true;
+
+// Add a simple marker
+const redPin = viewer.entities.add({
+    name: 'Red pin',
+    position: Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
+    billboard: {
+        image: 'https://img.icons8.com/color/48/000000/marker.png',
+        verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+    }
+});
+
+// Add terrain
+viewer.terrainProvider = Cesium.createWorldTerrain({
+    requestVertexNormals: true,
+    requestWaterMask: true
+});
+
+// Rotate the globe
+function rotateGlobe() {
+    viewer.scene.camera.rotate(Cesium.Cartesian3.UNIT_Z, -0.001);
+    requestAnimationFrame(rotateGlobe);
+}
+rotateGlobe();
